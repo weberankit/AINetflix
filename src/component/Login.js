830f505d5e8ebc8df1faafc598941dc219b9e6ea
ANKIT KUMAR <codingank@gmail.com>
2023-12-13 11:26:00 +0530

    validation done

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,21 @@
 import Header from "./Header"
-import {useState} from "react" 
+import {useState , useRef} from "react" 
+import { checkvalidate } from "../utils/validate"
 const Login=()=>{
-    const [isLogin , setisLogin]=useState(true)
+//const errorMsg=useRef("ok")  //not using this as its not reflect update in ui because it doesnot rerender
+const [errorMsg, setErrorMsg]=useState()
+const email=useRef(null)
+const password=useRef(null)
+const [isLogin , setisLogin]=useState(true)
 const toggleSign=()=>{
 setisLogin(!isLogin)
+}
+const handleCheckvalidate=()=>{
+console.log(email.current.value ,password)
+const message =checkvalidate(email.current.value , password.current.value)
+//using usref to store value
+//errorMsg.current=message
+setErrorMsg(message)
 }
 
     return(
@@ -17,13 +29,14 @@ setisLogin(!isLogin)
      </img>
 </div>
 
-<form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0  text-white bg-opacity-80 rounded-lg" >
+<form onSubmit={(e)=>e.preventDefault()} className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0  text-white bg-opacity-80 rounded-lg" >
     <h1>{isLogin?"Sign In":"Sign Up"}</h1>
 {!isLogin && <input type="text" className="py-4 my-4 w-full bg-gray-600 rounded-lg" placeholder="  Full Name"/>
 }
-<input type="text" className="py-4 my-4 w-full bg-gray-600 rounded-lg" placeholder="  Email Address"/>
-<input type="password" className="py-4 w-full my-4 bg-gray-600 rounded-lg" placeholder="  password"/>
-<button className="py-6 my-6 bg-red-700 w-full rounded-lg">{isLogin ? "Sign In" : "Sign Up"}</button>
+<input ref={email} type="text" className="py-4 my-4 w-full bg-gray-600 rounded-lg" placeholder="  Email Address"/>
+<input ref={password} type="password" className="py-4 w-full my-4 bg-gray-600 rounded-lg" placeholder="  password"/>
+<p className="text-red-600 font-bold">{errorMsg} </p>
+<button onClick={handleCheckvalidate} className="py-6 my-6 bg-red-700 w-full rounded-lg">{isLogin ? "Sign In" : "Sign Up"}</button>
 <p className="cursor-pointer" onClick={toggleSign}>{isLogin?"New to Netflix ? Sign Up now" : "Already user ? Sign In Now"}</p>
 </form>
 
