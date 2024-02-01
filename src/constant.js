d1678ed8c3fcfcad3cc17364d864f8659cfb2866
ANKIT KUMAR <codingank@gmail.com>
2024-02-01 10:11:46 +0530

    updating file

---
+++ src/constant.js
@@ -0,0 +1,55 @@
+export const logo="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
+
+export const userImg="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNkTHSQUApO8cjYAvxuIZ_Z5NJd5gFf3l3HKNE81PUwg&s" 
+/**/
+export const Api_options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjI4ODUyN2ZkZGRlMzMzODNiZDc2NTI4NzFkZWQyMSIsInN1YiI6IjY1OTBlYmVlYTU4OTAyNzExOTk2MjgzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u2i_w7wg22wPuQoI64D6Pbd0zuG1nngfrqXxr_HW3UE"  
+      //process.env.REACT_APP_TMDB_KEY
+    }
+  };
+  export const img_url="https://image.tmdb.org/t/p/w500"
+  export const backgroundImg="https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_medium.jpg" 
+
+  export const supported_languages=[
+    {identifier:"en",name:"English"},
+    {identifier:"hi",name:"हिंदी"},
+    {identifier:"bho",name:"भोजपुरी"},
+   // {//identifier:"bhojpuri",name:"Bhojpuri"},
+
+  ]
+
+
+  //constant file
+export function key(){
+  return "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"
+}
+
+  export const openaiKey=  "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"
+ // console.log("hi")
+    //console.log(searchValue?.current?.value)
+    /*
+    const gptQuery =
+    "Act as a Movie Recommendation system and suggest some movies for the query : " +
+    searchValue.current.value +
+    ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
+ 
+  const gptResults = await openai.chat.completions.create({
+    messages: [{ role: "user", content: gptQuery }],
+    model: "gpt-3.5-turbo",
+  });
+  console.log(gptResults)*/
+ //////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+ {
+  //<form className="w-1/2 bg-black grid grid-cols-12 " onSubmit={(e)=>e.preventDefault()}> </form>
+  
+              /*<input
+              ref={searchValue}
+               type="text" 
+            placeholder={}
+              className="p-4 m-4 col-span-9"
+              ></input>*/
+               }
+       
